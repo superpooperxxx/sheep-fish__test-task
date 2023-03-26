2766dbc4bc4ff950c8a0e3976565666acfaf3b10
superpooperxxx <superpooperxxx@users.noreply.github.com>
2023-03-26 20:54:47 +0000

    deploy: 14ad7143109be793392e91757f52c396d5648737

--- static/js/main.fd466267.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-typescript-starter-pack"]=this["webpackJsonpreact-typescript-starter-pack"]||[]).push([[0],{10:function(t,e,r){"use strict";r.r(e);var c=r(0),s=r.n(c),n=r(3),a=r.n(n),i=(r(8),r(1)),p=function(){return Object(i.jsx)("div",{className:"starter",children:"starter"})};a.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(p,{})}),document.getElementById("root"))},8:function(t,e,r){}},[[10,1,2]]]);
-//# sourceMappingURL=main.fd466267.chunk.js.map
