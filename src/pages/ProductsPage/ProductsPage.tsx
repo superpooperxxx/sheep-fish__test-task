1610bd7df6168169403d5d8fcbf87f7ab15c2b17
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-27 10:34:01 +0300

    added header to product page

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Header as ProductPageHeader } from '../../components/ProductsPage/Header';
 import './ProductsPage.scss';
 
 export const ProductsPage: React.FC = () => {
   return (
-    <main className="products-page">
+    <div className="products-page">
       <div className="container">
-        <div>Header: Title + New Product Btn</div>
-        <div>Search + Filter btn</div>
-        <div>Products</div>
-        <div>Pagination btn</div>
+        <ProductPageHeader />
+        <main>
+          <div>Search + Filter btn</div>
+          <div>Products</div>
+          <div>Pagination btn</div>
+        </main>
       </div>
-    </main>
+    </div>
   );
 };
