090784d66b97f46d7b0c5940e742b73ad3be714e
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-28 12:07:33 +0300

    small form fixes

--- src/components/ProductsPage/NewProductForm/schemas/index.ts
+++ src/components/ProductsPage/NewProductForm/schemas/index.ts
@@ -4,13 +4,13 @@ import * as yup from 'yup';
 export const basicSchema = yup.object().shape({
   title: yup
     .string()
-    .matches(/^[A-Za-z]+$/, 'Letters only')
+    .matches(/^[A-Za-zА-яа-я]+$/, 'Letters only')
     .min(3, 'Min 3 letters')
     .max(30, 'Max 30 letters')
     .required('Required'),
   author: yup
     .string()
-    .matches(/^[A-Za-z]+$/, 'Letters only')
+    .matches(/^[A-Za-zА-яа-я]+$/, 'Letters only')
     .min(3, 'Min 3 letters')
     .max(30, 'Max 30 letters')
     .required('Required'),
