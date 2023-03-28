9954fbcf7c37f6afee9446665ce0b813abd963d2
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-28 12:21:23 +0300

    added not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import './NotFoundPage.scss';
+import { useNavigate } from 'react-router-dom';
+import img from '../../static/images/image__not-found.png';
 
 export const NotFoundPage: React.FC = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="not-found">
+      <div className="container not-found__container">
+        <img
+          className="not-found__img"
+          src={img}
+          alt="not found illustration"
+        />
+        <button
+          type="button"
+          className="btn"
+          onClick={() => navigate('/')}
+        >
+          Home Page
+        </button>
+      </div>
+    </main>
+  );
 };
